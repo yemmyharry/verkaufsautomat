e5c3769c56ed8474fc048098696ac20af8333416
yemmyharry <yemmyharry@gmail.com>
2022-09-11 15:19:57 +0100

    Made changes to models

--- internal/core/domain/resource/machine.go
+++ internal/core/domain/resource/machine.go
@@ -1,34 +1,31 @@
 package resource
 
-import uuid "github.com/satori/go.uuid"
-
 type Product struct {
-	ProductID       uuid.UUID `gorm:"primary_key" json:"product_id"`
-	AmountAvailable int       `json:"amount_available"`
-	Cost            int       `json:"cost"`
-	ProductName     string    `json:"product_name"`
-	SellerID        uuid.UUID `json:"seller_id" gorm:"foreign_key:UserID"`
+	ProductID       uint   `json:"product_id" gorm:"primary_key;autoIncrement"`
+	AmountAvailable int    `json:"amount_available"`
+	Cost            int    `json:"cost"`
+	ProductName     string `json:"product_name"`
+	SellerID        uint   `json:"seller_id" gorm:"foreignKey:UserID"`
 }
 
 type User struct {
-	UserID   uuid.UUID `gorm:"primary_key" json:"user_id"`
-	Username string    `json:"username"`
-	Password string    `json:"password"`
-	Deposit  int       `json:"deposit"`
-	RoleID   uuid.UUID `json:"role_id" gorm:"foreign_key:RoleId"`
+	UserID   uint   `json:"user_id" gorm:"primaryKey;autoIncrement"`
+	Username string `json:"username" gorm:"not null;unique"`
+	Password string `json:"password" gorm:"not null"`
+	RoleID   uint   `json:"role_id" gorm:"foreignKey:RoleId"`
 }
 
 type Role struct {
-	RoleId   uuid.UUID `gorm:"primary_key" json:"role_id"`
-	RoleName string    `json:"role_name"`
+	RoleId   uint   `json:"roleId" gorm:"primaryKey;autoIncrement"`
+	RoleName string `json:"role_name"`
 }
 
 type Permission struct {
-	PermissionID   uuid.UUID `gorm:"primary_key" json:"permission_id"`
-	PermissionName string    `json:"permission_name"`
+	PermissionId   uint   `json:"permissionId" gorm:"primaryKey;autoIncrement"`
+	PermissionName string `json:"permission_name"`
 }
 
 type RolePermission struct {
-	RoleID       uuid.UUID `json:"role_id" gorm:"foreign_key:RoleId"`
-	PermissionID uuid.UUID `json:"permission_id" gorm:"foreign_key:PermissionID"`
+	RoleID       uint `json:"role_id" gorm:"foreignKey:RoleId"`
+	PermissionID uint `json:"permission_id" gorm:"foreignKey:PermissionId"`
 }
