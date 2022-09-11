e1a53c47a01446a5d0f0a6777d7d7cb63996d915
yemmyharry <yemmyharry@gmail.com>
2022-09-11 19:37:43 +0100

    Add create, get all products and get product by id routes

--- internal/adapter/api/resource/router.go
+++ internal/adapter/api/resource/router.go
@@ -1,17 +1,31 @@
 package resource
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func (s *HTTPHandler) Routes(router *gin.Engine) {
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*", "http://localhost:8080"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	apirouter := router.Group("api/v1")
 	apirouter.GET("/healthcheck", s.HealthCheck)
 	apirouter.POST("/register", s.Register)
 	apirouter.POST("/login", s.Login)
 
-	auth := apirouter.Group("/auth")
+	auth := router.Group("/auth")
 	auth.Use(s.AuthMiddleware())
-
+	auth.POST("/create_product", s.CreateProduct)
+	auth.GET("/get_products", s.GetProducts)
+	auth.GET("/get_product/:id", s.GetProduct)
 	router.NoRoute(func(c *gin.Context) { c.JSON(404, "no route") })
 }
