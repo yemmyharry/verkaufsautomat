b711357717d1ba4a5f9169f433bef3058131541f
yemmyharry <yemmyharry@gmail.com>
2022-09-11 15:23:57 +0100

    Add register and login methods to interface

--- internal/ports/resource/repository.go
+++ internal/ports/resource/repository.go
@@ -1,5 +1,9 @@
 package ports
 
+import "verkaufsautomat/internal/core/domain/resource"
+
 type MachineRepository interface {
 	HealthCheck() error
+	Register(user *resource.User) error
+	Login(user *resource.User) error
 }
--- internal/ports/resource/service.go
+++ internal/ports/resource/service.go
@@ -1,5 +1,9 @@
 package ports
 
+import "verkaufsautomat/internal/core/domain/resource"
+
 type MachineService interface {
 	HealthCheck() error
+	Register(user *resource.User) error
+	Login(user *resource.User) error
 }
