77f1cfb58f0b9f07b9294c075758d6753262b9cc
yemmyharry <yemmyharry@gmail.com>
2022-09-11 15:24:28 +0100

    Add register and login methods

--- internal/core/services/resource/service.go
+++ internal/core/services/resource/service.go
@@ -1,11 +1,16 @@
 package services
 
 import (
+	"verkaufsautomat/internal/core/domain/resource"
 	ports "verkaufsautomat/internal/ports/resource"
 )
 
 type service struct {
 	MachineRepository ports.MachineRepository
+}
+
+func (s service) Login(user *resource.User) error {
+	return s.MachineRepository.Login(user)
 }
 
 func (s service) HealthCheck() error {
@@ -16,4 +21,8 @@ func New(MachineRepository ports.MachineRepository) *service {
 	return &service{
 		MachineRepository: MachineRepository,
 	}
+}
+
+func (s service) Register(user *resource.User) error {
+	return s.MachineRepository.Register(user)
 }
