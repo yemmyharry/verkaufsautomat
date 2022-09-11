ac726f2623d2afed4e1c19683b77f7fc8b47f4c5
yemmyharry <yemmyharry@gmail.com>
2022-09-11 15:24:53 +0100

    Add auth middleware method to server

--- internal/adapter/api/resource/server.go
+++ internal/adapter/api/resource/server.go
@@ -1,11 +1,24 @@
 package resource
 
 import (
+	"github.com/gin-gonic/gin"
 	ports "verkaufsautomat/internal/ports/resource"
 )
 
 type HTTPHandler struct {
 	MachineService ports.MachineService
+}
+
+func (s *HTTPHandler) AuthMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		err := TokenValid(c)
+		if err != nil {
+			c.JSON(401, "Unauthorized")
+			c.Abort()
+			return
+		}
+		c.Next()
+	}
 }
 
 func NewHTTPHandler(MachineService ports.MachineService) *HTTPHandler {
