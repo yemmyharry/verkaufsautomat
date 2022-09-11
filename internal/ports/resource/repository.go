63f4ae8a3ff980322575bf6d596a0e3598630d52
yemmyharry <yemmyharry@gmail.com>
2022-09-11 19:36:40 +0100

    Add create, get all products and get product by id to interface

--- internal/ports/resource/repository.go
+++ internal/ports/resource/repository.go
@@ -6,4 +6,7 @@ type MachineRepository interface {
 	HealthCheck() error
 	Register(user *resource.User) error
 	Login(user *resource.User) error
+	CreateProduct(product *resource.Product) error
+	GetProducts() ([]resource.Product, error)
+	GetProductById(id int) (resource.Product, error)
 }
