685ca79b3a114b190432fbf0f7f126946819e70e
yemmyharry <yemmyharry@gmail.com>
2022-09-11 15:25:21 +0100

    Add register,login and auth routes

--- internal/adapter/api/resource/router.go
+++ internal/adapter/api/resource/router.go
@@ -7,5 +7,11 @@ import (
 func (s *HTTPHandler) Routes(router *gin.Engine) {
 	apirouter := router.Group("api/v1")
 	apirouter.GET("/healthcheck", s.HealthCheck)
+	apirouter.POST("/register", s.Register)
+	apirouter.POST("/login", s.Login)
+
+	auth := apirouter.Group("/auth")
+	auth.Use(s.AuthMiddleware())
+
 	router.NoRoute(func(c *gin.Context) { c.JSON(404, "no route") })
 }
