b43d67bca049109d8a6b764e624e9d165abe7b63
yemmyharry <yemmyharry@gmail.com>
2022-09-12 21:50:40 +0100

    Add test for deposit

---
+++ internal/adapter/api/resource/handler_test.go
@@ -0,0 +1,55 @@
+package resource
+
+import (
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"github.com/golang/mock/gomock"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	services "verkaufsautomat/internal/core/services/mock"
+)
+
+func TestApplication_Deposit(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	mockedService := services.NewMockMachineService(ctrl)
+	handler := NewHTTPHandler(mockedService)
+
+	router := gin.Default()
+
+	handler.Routes(router)
+
+	t.Run("Deposit money", func(t *testing.T) {
+		deposit := struct {
+			Amount int `json:"amount"`
+		}{
+			Amount: 100,
+		}
+		token := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2NjMwMTE3MDcsInJvbGVfaWQiOjEsInVzZXJfaWQiOjEsInVzZXJuYW1lIjoiam9obiJ9.jbgrs1gySB42QzaoCTCtptzk4Vll_de_LMLzjleSd3E"
+		mockedService.EXPECT().DepositMoney(1, deposit.Amount).Return(nil)
+		m, _ := json.Marshal(deposit)
+		req, err := http.NewRequest("PATCH", "/auth/deposit_money", strings.NewReader(string(m)))
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", "Bearer "+token)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		response := httptest.NewRecorder()
+		router.ServeHTTP(response, req)
+
+		if strings.Contains(response.Body.String(), "money deposited") {
+			t.Log("Deposit money test passed")
+		} else {
+			t.Error("Deposit money test failed")
+		}
+
+		if response.Code != http.StatusOK {
+			t.Errorf("Expected status code %d, got %d", http.StatusOK, response.Code)
+		}
+
+	})
+
+}
